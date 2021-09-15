616c447bdab4308955d73ba06c95000ea85d0fed
cbakcoleman <88118758+cbakcoleman@users.noreply.github.com>
2021-09-15 11:47:17 -0700

    add reg and login

---
+++ controllers/users.py
@@ -0,0 +1,57 @@
+from recipes_app import app
+from flask import render_template, redirect, request, session
+from flask import flash
+from recipes_app.models.user import User
+from flask_bcrypt import Bcrypt
+
+bcrypt = Bcrypt(app)
+
+@app.route('/')
+def home():
+    return render_template('reg_login.html')
+
+@app.route('/register', methods=['post'])
+def add_user():
+    if not User.validate_user(request.form):
+        return redirect('/')
+
+    password_hash = bcrypt.generate_password_hash(request.form['password'])
+    data = {
+        'first_name' : request.form['first_name'],
+        'last_name' : request.form['last_name'],
+        'email' : request.form['email'],
+        'password' : password_hash
+    }
+
+    user_id = User.add_user(data)
+    session['user_id'] = user_id
+
+    return redirect(f'/success/{user_id}')
+
+@app.route('/login', methods=['post'])
+def login():
+    data = { "email" : request.form["email"] }
+    user = User.get_by_email(data)
+
+    if not user:
+        flash('Invalid login entry!', 'login')
+        return redirect('/')
+    if not bcrypt.check_password_hash(user.password, request.form['password']):
+        flash('Invalid login entry!', 'login')
+        return redirect('/')
+
+    session['user_id'] = user.id
+
+    return redirect(f'/success/{session["user_id"]}')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
+
+@app.route('/success/<id>')
+def success(id):
+    if not session['user_id']:
+        return redirect('/')
+    user = User.get_by_id({'id' :id}) 
+    return render_template('success.html', user = user)
---
+++ models/user.py
@@ -0,0 +1,60 @@
+from recipes_app.config.mysqlconnection import connectToMySQL
+from flask import flash
+import re
+
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+PASSWORD_REGEX = re.compile(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$")
+
+class User:
+    def __init__(self, data):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.password = data['password']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+    @staticmethod
+    def validate_user(user):
+        is_valid = True
+        if not EMAIL_REGEX.match(user['email']):
+            flash('Invalid email address!', 'email')
+            is_valid = False
+        if len(user['first_name']) < 2:
+            flash('First name must be at least 2 characters!', 'first_name')
+            is_valid = False
+        if len(user['last_name']) < 2:
+            flash('Last name must be at least 2 characters!', 'last_name')
+            is_valid = False
+        if not PASSWORD_REGEX.match(user['password']):
+            flash('Password must be at least 8 characters, include at least one uppercase letter, one lowercase letter, one number, and one special character!', 'password')
+        if user['confirm_password'] != user['password']:
+            flash('Confirm password does not match password!', 'confirm_password')
+            is_valid = False
+        return is_valid
+        
+    @classmethod
+    def add_user(cls, data):
+        query = 'INSERT INTO users (first_name, last_name, email, password) VALUES ( %(first_name)s, %(last_name)s , %(email)s , %(password)s );'
+        new_user = connectToMySQL("loginAndReg_schema").query_db(query, data)
+        return new_user
+
+    @classmethod
+    def get_by_id(cls, data):
+        query = 'SELECT * FROM users WHERE id = %(id)s ;'
+        results = connectToMySQL('loginAndReg_schema').query_db(query, data)
+        return cls(results[0])
+
+    @classmethod
+    def get_by_email(cls, data):
+        query = 'SELECT * FROM users WHERE email = %(email)s'
+        results = connectToMySQL('loginAndReg_schema').query_db(query, data)
+
+        if len(results) < 1:
+                return False
+
+        return cls(results[0])
+
+
+        
